4b78f7271b6843f8d757c78868f0baa6e33ce0a2
gkulkarni222 <gayatrikulkarni2222@gmail.com>
2024-06-21 10:47:36 +0530

    new java class added

---
+++ src/selenium/TestPage.java
@@ -0,0 +1,15 @@
+package selenium;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestPage t = new TestPage();
+		t.test();
+	}
+
+	public void test() {
+		System.out.println("test");
+	}
+
+}
